f712f158b343e4ed9129b6a5d355f63cb4c4bc6a
moon622 <jamie.moon@pnnl.gov>
2019-07-11 17:29:50 -0700

    Add function for reading MSMS data from DMS

--- R/dms_read_msgf_data.R
+++ R/dms_read_msgf_data.R
@@ -1,8 +1,9 @@
 #' Reading MSGF Results. Generic.
 #'
-#' Reading MSGF output from a single directory
+#' Reading MSGF output from PNNL's DMS
 #'
 #' @param path_to_MSGF_results (path string) to directory with MSGF results for all datasets
+#' @param DataPkgNumber (Numeric or Character vector) containing Data Package ID(s) located in DMS
 #' @return (MSnID) MSnID object
 #' @importFrom dplyr mutate
 #' @importFrom MSnID MSnID
@@ -14,18 +15,34 @@
 #' head(MSnID::psms(msnid))
 
 #' @export
-read_msgf_data <- function(path_to_MSGF_results){
-   msnid <- MSnID(".")
-   # accession -> Protein
-   # calculatedMassToCharge -> f(MH, Charge) MSnID:::.PROTON_MASS
-   # (MH + (Charge-1)*MSnID:::.PROTON_MASS)/Charge
-   # chargeState -> Charge
-   # experimentalMassToCharge -> PrecursorMZ
-   # isDecoy -> grep("^XXX", Protein)
-   # peptide -> Peptide
-   # spectrumFile -> Dataset
-   # spectrumID -> Scan
-   x <- collate_files(path_to_MSGF_results, "_msgfplus_syn.txt") %>%
+dms_read_msms_data <- function(path_to_MSGF_results, DataPkgNumber = NULL){
+  msnid <- MSnID(".")
+  # accession -> Protein
+  # calculatedMassToCharge -> f(MH, Charge) MSnID:::.PROTON_MASS
+  # (MH + (Charge-1)*MSnID:::.PROTON_MASS)/Charge
+  # chargeState -> Charge
+  # experimentalMassToCharge -> PrecursorMZ
+  # isDecoy -> grep("^XXX", Protein)
+  # peptide -> Peptide
+  # spectrumFile -> Dataset
+  # spectrumID -> Scan
+  
+  if (!is.null(DataPkgNumber)) {
+    
+    # Fetch job records for data package(s)
+    if (length(DataPkgNumber) > 1) {
+      job_rec_ls <- lapply(DataPkgNumber, get_job_records_by_dataset_package)
+      jobRecords <- Reduce(rbind, job_rec_ls)
+    }
+    
+    else {
+      jobRecords <- get_job_records_by_dataset_package(DataPkgNumber)
+    }
+    
+    jobRecords <- jobRecords[grepl("MSGFPlus", jobRecords$Tool),]
+    
+    x <- get_results_for_multiple_jobs.dt(jobRecords)
+    x <- x %>% 
       mutate(accession = Protein,
              calculatedMassToCharge = (MH + (Charge-1)*MSnID:::.PROTON_MASS)/Charge,
              chargeState = Charge,
@@ -34,17 +51,35 @@ read_msgf_data <- function(path_to_MSGF_results){
              peptide = Peptide,
              spectrumFile = Dataset,
              spectrumID = Scan)
-   # extra piece that may be removed in the future.
-   # decoy accessions stripped of XXX_ prefix
-   # x <- x %>%
-   #    mutate(accession = sub("^XXX_","",accession))
-
-   # clean peptide sequences
-   x <- mutate(x, pepSeq = MSnID:::.get_clean_peptide_sequence(peptide))
-
-   psms(msnid) <- x
-
-   return(msnid)
+    
+    x <- mutate(x, pepSeq = MSnID:::.get_clean_peptide_sequence(peptide))
+    
+    psms(msnid) <- x
+    return(msnid)
+  }
+  
+  else{
+    x <- collate_files(path_to_MSGF_results, "_msgfplus_syn.txt") %>%
+      mutate(accession = Protein,
+             calculatedMassToCharge = (MH + (Charge-1)*MSnID:::.PROTON_MASS)/Charge,
+             chargeState = Charge,
+             experimentalMassToCharge = PrecursorMZ,
+             isDecoy = grepl("^XXX", Protein),
+             peptide = Peptide,
+             spectrumFile = Dataset,
+             spectrumID = Scan)
+    # extra piece that may be removed in the future.
+    # decoy accessions stripped of XXX_ prefix
+    # x <- x %>%
+    #    mutate(accession = sub("^XXX_","",accession))
+    
+    # clean peptide sequences
+    x <- mutate(x, pepSeq = MSnID:::.get_clean_peptide_sequence(peptide))
+    
+    psms(msnid) <- x
+    
+    return(msnid)
+  }
 }
 
 
