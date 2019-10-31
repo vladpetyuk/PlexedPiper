daca1af2c11dd6c68319be2cb846fd1e34f33083
Vlad Petyuk <petyuk@gmail.com>
2019-10-31 10:02:57 -0700

    udpated docs

---
+++ vignettes/tmt_pipeline_for_PNNL_DMS.R
@@ -0,0 +1,8 @@
+## ----setup, echo=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(message=FALSE, warning=FALSE)
+# knitr::opts_chunk$set(echo=T, message=F, warning=F, fig.align='center', out.width='10cm')
+
+## ----libraries-------------------------------------------------------------
+library(PlexedPiper)
+library(MSnID)
+
